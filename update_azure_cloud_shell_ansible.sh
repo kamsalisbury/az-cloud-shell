fb6db8eedc65f39839a16065ec36a5f94f5fc87d
Kam Salisbury <29674098+kamsalisbury@users.noreply.github.com>
2024-07-02 14:19:23 -0400

    Update update_azure_cloud_shell_ansible.sh

--- update_azure_cloud_shell_ansible.sh
+++ update_azure_cloud_shell_ansible.sh
@@ -3,4 +3,4 @@ ansible-galaxy collection install azure.azcollection --force
 pip3 install -r ~/.ansible/collections/ansible_collections/azure/azcollection/requirements.txt
 
 # Then, add Ansible roles used most often. Searching for additional role example: 'ansible-galaxy search alpine'
-ansible-galaxy install andrewrothstein.alpine-glibc-shim
+# ansible-galaxy collection install andrewrothstein.alpine-glibc-shim
