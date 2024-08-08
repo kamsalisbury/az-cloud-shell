18da1e50834eba43368678a57178f1fc57d08fe1
Kam Salisbury <29674098+kamsalisbury@users.noreply.github.com>
2024-08-08 08:10:05 -0400

    Delete update_azure_cloud_shell_ansible.sh

--- update_azure_cloud_shell_ansible.sh
+++
@@ -1,6 +0,0 @@
-# First, fix ansible because it is not completely installed in Azure Cloud Shell
-ansible-galaxy collection install azure.azcollection --force
-pip3 install -r ~/.ansible/collections/ansible_collections/azure/azcollection/requirements.txt
-
-# Then, add Ansible roles used most often. Searching for additional role example: 'ansible-galaxy search alpine'
-# ansible-galaxy collection install andrewrothstein.alpine-glibc-shim
