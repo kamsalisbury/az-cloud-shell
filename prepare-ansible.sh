9ce1574412b5700f8621f16b16c1157f58e91ff8
Kam Salisbury <29674098+kamsalisbury@users.noreply.github.com>
2024-08-08 08:10:57 -0400

    Update prepare-ansible.sh

--- prepare-ansible.sh
+++ prepare-ansible.sh
@@ -1,6 +1,14 @@
-# assuming bash shell
+# assuming bash shell, though powershell has worked
+
+# First, fix ansible because it is not completely installed-updated in Azure Cloud Shell
+#ansible-galaxy collection install azure.azcollection --force
+#pip3 install -r ~/.ansible/collections/ansible_collections/azure/azcollection/requirements.txt
 pip install ansible[azure]
 ansible-galaxy role install azure.azure_preview_modules
 pip install -r ~/.ansible/roles/azure.azure_preview_modules/files/requirements-azure.txt
+
+# Then, add Ansible roles used most often. Searching for additional role example: 'ansible-galaxy search alpine'
+# ansible-galaxy collection install andrewrothstein.alpine-glibc-shim
+
 # Pull samples via git
 git clone https://github.com/Azure-Samples/ansible-playbooks.git
